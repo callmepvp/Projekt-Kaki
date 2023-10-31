c29ecdb7b2941fb0d2afcce8615391ec7399fb78
Kuulimagnaat <kaarelparve@gmail.com>
2023-10-31 16:20:31 +0200

    Text debugging

--- Tekst.py
+++ Tekst.py
@@ -15,6 +15,9 @@ def Silbita(tekst):
 
 # Ingliskeelne kood copy pastetud kusagilt lehelt, aga need funktsioonid teevad täpselt seda, mida vaja. Jagab antud teksti (stringi) tükkideks nii, et valitud fondi korral ei ületaks ükski tükk kindlat valitud laiust pikslites. Lühidalt jagab teksti tükkideks, et iga tüki saaks eraldi reale renderdada ja kõik oleksid roughly reapikkused.
 
+
+
+
 def truncline(text, font, maxwidth):
     real=len(text)       
     stext=text           
@@ -22,7 +25,6 @@ def truncline(text, font, maxwidth):
     cut=0
     a=0                  
     done=1
-    old = None
     while l > maxwidth:
         a=a+1
         n=text.rsplit(None, a)[0]
@@ -46,12 +48,24 @@ def wrapline(text, font, maxwidth):
         text=text[nl:]                                 
     return wrapped
 
+# Moodustab nimekirja sõnedest, mis on ühe rea jaoks sobiva pikkusega.
 def wrap_multi_line(text, font, maxwidth):
     """ returns text taking new lines into account.
     """
     lines = chain(*(wrapline(line, font, maxwidth) for line in text.splitlines()))
     return list(lines)
 
+
+def EraldaSobivaPikkusegaTekst(algtekst, sobivPikkus, fontObject):
+    kasvatatav = ""
+    täheVõtuKoht = 0
+    while True:
+        tekstilaius = fontObject.size(kasvatatav)[0]
+        if tekstilaius > sobivPikkus or kasvatatav == algtekst:
+            return kasvatatav, algtekst[täheVõtuKoht:]
+        else:
+            kasvatatav += algtekst[täheVõtuKoht]
+            täheVõtuKoht += 1
 
 
 class Tekst:
@@ -60,24 +74,25 @@ class Tekst:
         self.font = pygame.font.Font(font, suurus)
         self.pind = pind
         self.asuk = asuk
-        self.img = self.font.render(tekst, True, värv)
+        self.tekst = tekst
         self.värv = värv
     
     
     def Joonista(self):
         suurtäheKõrgus = self.font.get_ascent()
         
-        # Oke, see on scuffed ja töötab kindlalt ainult selle fondiga. 0.34 on silma järgi valitud, aga see arv aitab teha nii, et tekst joonistuks valitud kohta täpselt nii, et väiketähe kõrguse keskkoht oleks soovitud kõrgusel. Keskkoht selle pärast, et ss on bullet pointe lihtsam joondada. Seda peaks vist täiendama, kui on vaja kirjutada mitmerealine tekst. Scuffed on veel see, et height on fontide terminoloogias väiketähe kõrgus, aga siin imo on vahemik nt j tähe langusest l tähe tõusuni, seega u 2x väiketähe kõrgus.
+        # Oke, see on scuffed ja töötab kindlalt ainult selle fondiga. 0.34 on silma järgi valitud, aga see arv aitab teha nii, et tekst joonistuks valitud kohta täpselt nii, et väiketähe kõrguse keskkoht oleks soovitud kõrgusel. Keskkoht selle pärast, et ss on bullet pointe lihtsam joondada. Seda peaks vist täiendama, kui on vaja kirjutada mitmerealine tekst. Scuffed on veel see, et height on fontide terminoloogias väiketähe kõrgus, aga siin imo on vahemik nt j tähe langusest l tähe tõusuni, seega u 2x väiketähe kõrgus. 0.34 ≈ 0.25 lol
         poolVäiketäheKõrgus = self.font.get_height()*0.34
         
 
         tekstiAsuk = (self.asuk[0], self.asuk[1]-suurtäheKõrgus + poolVäiketäheKõrgus)
 
-        self.pind.blit(self.img, tekstiAsuk)
+        img = self.font.render(self.tekst, True, self.värv)
+        self.pind.blit(img, tekstiAsuk)
 
 class MitmeReaTekst:
 
-    def __init__(self, pind, tekst="Lorem Ipsum", reaLaius=400, font="CORBEL.TTF", värv=(255,0,0), asuk=(0,0), suurus=60, reavahe=50):
+    def __init__(self, pind, tekst="Lorem Ipsum", reaLaius=400, font="CORBEL.TTF", värv=(255,0,0), asuk=(0,0), suurus=60, reavahe=50, mituRidaJoonistada=1):
         self.font = font
         print(os.path.join('Fondid', font))
         self.pygfont = pygame.font.Font(font, suurus)
@@ -89,10 +104,11 @@ class MitmeReaTekst:
         self.reaLaius = reaLaius
         self.reavahe = reavahe
         self.suurus = suurus
+        self.mituRidaJoonistada = mituRidaJoonistada
     
     
     def Joonista(self):
-        read = wrap_multi_line(self.tekst, self.pygfont, self.reaLaius)
+        read = wrap_multi_line(self.tekst, self.pygfont, self.reaLaius)[0:self.mituRidaJoonistada]
 
         mitmes = 0
         for i in read:
--- index.py
+++ index.py
@@ -2,28 +2,39 @@
 from Kujundid import *
 from Tekst import *
 
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
     clock = pygame.time.Clock()
 
     pikktekst = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
-    tekst1 = MitmeReaTekst(screen, pikktekst, suurus=30, reaLaius=500, asuk=(0, 600), reavahe=30, font="Fondid/Gogh-ExtraBold.ttf", värv=(10,10,10))
-
+    fontObject = pygame.font.Font("Fondid\Gogh-ExtraBold.ttf", 20)
+    tekst1 = Tekst(screen, "", "Fondid\Gogh-ExtraBold.ttf", (10,10,10), (0,100), 60)
+    tekst2 = Tekst(screen, "", "Fondid\Gogh-ExtraBold.ttf", (10,10,10), (0,200), 60)
+    
     running = True
     while running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+
                 running = False
 
         screen.fill("purple")
         
 
 
+
         hiireAsuk = pygame.mouse.get_pos()
-        tekst1.MuudaReaLaiust(hiireAsuk[0])
+        pygame.draw.circle(screen, "black", hiireAsuk, 10)
+        tekstid = EraldaSobivaPikkusegaTekst(pikktekst, hiireAsuk[0], fontObject)
+        print(tekstid[0])
+        tekst1.tekst = tekstid[0]
+        tekst2.tekst = tekstid[1]
+        
+        tekst1.Joonista()
+        tekst2.Joonista()
+
 
 
         
