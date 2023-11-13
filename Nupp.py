60bf52a17559238c27cca0225b6b8aee1cdb20f9
Kuulimagnaat <kaarelparve@gmail.com>
2023-11-13 22:04:52 +0200

    cleanup

---
+++ Nupp.py
@@ -0,0 +1,13 @@
+class LisaSündmuseNupp:
+    def __init__(olek, pind):
+        self.olek = olek
+        self.pind = pind
+        self.asukoht = (0,0)
+        self.suurus = (200, 80)
+
+
+
+    def Joonista():
+        pildiOsa = 0.2
+        kiri = "Lisa sündmus"
+        pildiOsaLaius = self.suurus[0]*pildiOsa
