90a0a29600c72737d21f7c26f2af6dc62e669a83
callmepvp <mrxgameing@gmail.com>
2023-11-16 17:38:05 +0200

    ID genereerimine

--- UtilityFunktsioonid.py
+++ UtilityFunktsioonid.py
@@ -1,23 +1,27 @@
 ﻿import pygame
+from Programmiolek import ProgrammiOlek
 from PIL import Image
 
-"""
-def find_smallest_missing_number(nums):
-    sorted_nums = sorted(nums)
+def GenereeriID(Olek: "ProgrammiOlek"):
+    Nimekiri = Olek.sündmusteNimekiri
+    #* kontrolli pikkust
+    
+    if len(Nimekiri) != 1:
+        kasutatudID = [sündmus.id for sündmus in Nimekiri]
+        sorteeritudID = sorted(kasutatudID)
 
-    smallest_missing = 1
-    for num in sorted_nums:
-        if num == smallest_missing:
-            smallest_missing += 1
-        elif num > smallest_missing:
-            break
+        vähimPuudu = 1
+        for num in sorteeritudID:
+            if num == vähimPuudu:
+                vähimPuudu += 1
+            elif num > vähimPuudu:
+                break
 
-    return smallest_missing
-
-print(find_smallest_missing_number([1, 2, 3, 4, 5, 6, 7]))"""
-
-
-
+        print(vähimPuudu)
+        return vähimPuudu
+    else:
+        return 1
+    
 # Teeb PIL image objekti pygame surface objektiks.
 def PILpiltPinnaks(PILpilt):
     image_data = PILpilt.tobytes()
