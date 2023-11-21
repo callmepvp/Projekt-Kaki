4f0822fb8b359b620288f17f960bdd77d49b38a2
Kuulimagnaat <kaarelparve@gmail.com>
2023-11-21 18:29:43 +0200

    Töötav hiir

--- Programm.py
+++ Programm.py
@@ -65,7 +65,6 @@ class Programm:
         
 
         def JoonistaAsjad():
-            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
             ekraan.fill((255, 255, 255, 255))
             
             """rk1Asuk = (0.1, 0.1)
